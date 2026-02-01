7a437a210c6c460f2019d0ce634d8b45b04dde2b
florinzgz <florinzgz@hotmail.com>
2026-02-01 10:29:50 +0100

    Add interrupt handlers header file (stm32g4xx_it.h)

--- Core/Inc/stm32g4xx_it.h
+++ Core/Inc/stm32g4xx_it.h
@@ -1,30 +1,18 @@
 /**
-  ******************************************************************************
+  ****************************************************************************
   * @file    stm32g4xx_it.h
   * @brief   Interrupt handlers header file
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  ******************************************************************************
+  ****************************************************************************
   */
 
 #ifndef __STM32G4xx_IT_H
 #define __STM32G4xx_IT_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
-/* Exported macro ------------------------------------------------------------*/
-/* Exported functions prototypes ---------------------------------------------*/
 
 void NMI_Handler(void);
 void HardFault_Handler(void);
@@ -35,18 +23,19 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
-
-/* Peripheral interrupt handlers */
-void EXTI0_IRQHandler(void);
-void EXTI1_IRQHandler(void);
-void EXTI2_IRQHandler(void);
 void EXTI4_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
-void FDCAN1_IT0_IRQHandler(void);
+void TIM1_UP_IRQHandler(void);
 void TIM2_IRQHandler(void);
+void TIM8_UP_IRQHandler(void);
+void FDCAN1_IT0_IRQHandler(void);
+void FDCAN1_IT1_IRQHandler(void);
+void I2C1_EV_IRQHandler(void);
+void I2C1_ER_IRQHandler(void);
+void ADC1_2_IRQHandler(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __STM32G4xx_IT_H */
+#endif
